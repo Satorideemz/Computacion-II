87a59e71baa64cceee547adf97a0a9e4e8e21955
Satori <bl00dztr34m@gmail.com>
2023-11-11 20:44:23 -0300

    tp funcionando

--- TP2/TP2.py
+++ TP2/TP2.py
@@ -1,26 +1,34 @@
 import socket
 from PIL import Image
 import io
+import multiprocessing
+import argparse
 
 def convert_to_grayscale_pillow(image_data):
     img = Image.open(io.BytesIO(image_data)).convert('L')
     return img
 
-def start_server():
+def image_processing_child(pipe, event):
+    while True:
+        image_data = pipe.recv()
+        if image_data == b'exit':
+            # Exit the child process
+            break
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        grayscale_image = convert_to_grayscale_pillow(image_data)
 
-    # Bind the socket to the port
-    server_address = ('127.0.0.1', 8085)
-    server_socket.bind(server_address)
+        with io.BytesIO() as output:
+            grayscale_image.save(output, format="JPEG")
+            grayscale_data = output.getvalue()
 
-    # Listen for incoming connections
-    server_socket.listen(1)
-    print(f'Server is starting on {server_address}')
+        pipe.send(grayscale_data)
 
-def handle_connection(client_address,client_socket):
+        # Notify the parent that the image conversion is complete
+        event.set()
 
+def handle_connection(client_socket, child_pipe, event):
     try:
+        client_address = client_socket.getpeername()
         print(f'Connection from {client_address}')
 
         # Receive the data in small chunks
@@ -40,14 +48,13 @@ def handle_connection(client_address,client_socket):
         method = request_lines[0].split(b' ')
 
         # Handle the request
-        handle_request(method,data,client_socket)
+        handle_request(method, data, client_socket, child_pipe, event)
 
     finally:
         # Clean up the connection
-        client_socket.close()    
+        client_socket.close()
 
-
-def handle_request(method,data,client_socket):
+def handle_request(method, data, client_socket, child_pipe, event):
     try:
         if method[0] == b'POST':
             print(f'POST request received. Converting to grayscale...')
@@ -59,15 +66,19 @@ def handle_request(method,data,client_socket):
             # Get the image data between start and end indices
             image_data = data[start_idx:end_idx]
 
-            # Convert the received image to grayscale
-            grayscale_image = convert_to_grayscale_pillow(image_data)
+            # Send the image data to the child process
+            child_pipe.send(image_data)
 
-            # Send the grayscale image back to the client
-            with io.BytesIO() as output:
-                grayscale_image.save(output, format="JPEG")
-                grayscale_data = output.getvalue()
+            # Wait for the child process to complete image conversion
+            event.wait()
 
-            # Send the correct HTTP/1.1 response
+            # Receive the processed grayscale image data from the child process
+            grayscale_data = child_pipe.recv()
+
+            # Reset the event for the next connection
+            event.clear()
+
+            # Send the correct HTTP/1.1 response with the grayscale image data
             response = f'HTTP/1.1 200 OK\r\nContent-Length: {len(grayscale_data)}\r\n\r\n'.encode('utf-8')
             client_socket.sendall(response + grayscale_data)
 
@@ -79,29 +90,45 @@ def handle_request(method,data,client_socket):
         error_message = b'Request refused by server\n'
         client_socket.sendall(response + error_message)
 
+def start_server(ip, port):
 
+    if ':' in ip:
+        # Use socket.AF_INET6 for IPv6 addresses
+        server_socket = socket.create_server((ip, port), family=socket.AF_INET6, dualstack_ipv6=True)
+    else:
+        # Use socket.AF_INET for IPv4 addresses
+        server_socket = socket.create_server(('0.0.0.0', port))
+    # Create an event for synchronization
+    conversion_event = multiprocessing.Event()
 
-def start_server():
-
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # Bind the socket to the port
-    server_address = ('127.0.0.1', 8082)
-    server_socket.bind(server_address)
-
-    # Listen for incoming connections
-    server_socket.listen(1)
-    print(f'Server is starting on {server_address}')
+    print(f'Server is starting on {server_socket.getsockname()}')
 
     while True:
         # Wait for a connection
         print('Waiting for a connection...')
-        client_socket, client_address = server_socket.accept()
-        #Handle the connection between server and client
-        handle_connection(client_address,client_socket)
+        client_socket, _ = server_socket.accept()
 
-def main():
-    start_server()
+        # Create a new child process for each connection
+        child_pipe, parent_pipe = multiprocessing.Pipe()
+        image_processing_process = multiprocessing.Process(
+            target=image_processing_child, args=(child_pipe, conversion_event))
+        image_processing_process.start()
+
+        # Handle the connection between server and client
+        handle_connection(client_socket, parent_pipe, conversion_event)
+
+        # Send 'exit' signal to child process to terminate it
+        parent_pipe.send(b'exit')
+        image_processing_process.join()
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Tp2 - procesa imagenes')
+    parser.add_argument('-i', '--ip', default='::', help='Direccion de escucha')
+    parser.add_argument('-p', '--port', type=int, default=8080, help='Puerto de escucha')
+
+    args = parser.parse_args()
+    
+    try:
+        start_server(args.ip, args.port)
+    except KeyboardInterrupt:
+        print('Server stopped.')
